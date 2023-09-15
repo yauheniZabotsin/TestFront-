21ba0caf2dbfb2e2f9432e5d874164f13354aeb1
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2023-09-15 20:22:24 +0300

    feat: add functional toggle page

--- js/index.js
+++ js/index.js
@@ -4,3 +4,31 @@ const ageBtn = document.querySelector(".age__btn-range");
 ageBtn.addEventListener("mouseover", () => {
   setInterval(() => (ageCurrent.textContent = ageBtn.value), 0);
 });
+
+const nextBtn = document.querySelectorAll(".wrap__btn-next");
+const breakBtn = document.querySelectorAll(".wrap__btn-break");
+const pages = document.querySelectorAll(".container");
+
+nextBtn.forEach((el) =>
+  el.addEventListener("click", (e) => {
+    let page = e.target.closest(".wrap__btn").id;
+
+    if (page == 0) {
+      pages[page].classList.add("hidden");
+      page++;
+      pages[page].classList.remove("hidden");
+    }
+  })
+);
+
+breakBtn.forEach((el) =>
+  el.addEventListener("click", (e) => {
+    let page = e.target.closest(".wrap__btn").id;
+
+    if (page == 1) {
+      pages[page].classList.add("hidden");
+      page--;
+      pages[page].classList.remove("hidden");
+    }
+  })
+);
